4dd35d59246f8592d8e37c47b4fbece41f7fdc7f
Saleem2001 <92034872+Saleem2001@users.noreply.github.com>
2023-01-19 23:58:15 +0530

    Added music player application

---
+++ simpleMusicPlayer.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import music from '@salesforce/resourceUrl/music';
+import poster from '@salesforce/resourceUrl/poster';
+export default class SimpleMusicPlayer extends LightningElement {
+    musicFile = music
+    musicPoster = poster
+}
